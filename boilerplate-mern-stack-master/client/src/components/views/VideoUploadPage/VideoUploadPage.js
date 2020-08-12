59a87fb51b6c509a469dc98a001433c2dd20c19f
ChoHJin <cjc9739@naver.com>
2020-08-12 16:50:08 +0900

    비디오 업로드 성공

--- boilerplate-mern-stack-master/client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,21 +1,22 @@
 import React, {useState} from 'react';
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import Dropzone from 'react-dropzone';
+import Axios from 'axios';
 
-const { TextArea } = Input;
+
 const { Title } = Typography; 
-
+const { TextArea } = Input;
 
 const PrivateOptions = [
-    {value: 0, label : "Private"},
-    {value: 1, label : "Public"}
+    { value: 0, label : "Private" },
+    { value: 1, label : "Public"  }
 ]
 
 const CategoryOptions = [
-    { value : 0, label: "Firm & Animation"},
-    { value : 1, label : "Autos & Vehicles"},
-    {value: 2, label : "Music"},
-    {value: 3, label : "Pets & Animals"}
+    { value : 0, label: "Firm & Animation" },
+    { value : 1, label : "Autos & Vehicles" },
+    { value: 2, label : "Music" },
+    { value: 3, label : "Pets & Animals" }
 ]
 
 
@@ -42,6 +43,25 @@ function VideoUploadPage() {
         setCategory(e.currentTarget.value);
     }
 
+    const onDrop = (files) => {
+
+        let formData = new FormData;
+        const config = { 
+              header: {'content-type' : 'multipart/form-data'}
+        }
+        formData.append("file", files[0])
+
+        Axios.post("/api/video/uploadfiles", formData, config)
+            .then((response) => {
+                if(response.data.success) {
+                    console.log(response.data)
+                }else {
+                    alert('비디오 업로드를 실패했습니다.')
+                }
+            });
+    };
+
+
     return (
         <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
                 <div style ={{ textAlign : 'center', marginBottom : '2rem' }}>
@@ -52,9 +72,10 @@ function VideoUploadPage() {
                     <div style={{ display:'flex', justifyContent:'space-between'}}>
                             {/*Drop Zone */}
                             <Dropzone
-                            onDrop
-                            multiple
-                            maxSize>
+                            onDrop={onDrop}
+                            multiple={false}
+                            maxSize={1000000000}
+                            >
                             {({ getRootProps, getInputProps}) =>(
                                 <div style = {{ width: '300px', height: '240px', border: '1px solid lightgray', display:'flex',
                                 alignItems:'center', justifyContent: 'center'}} {...getRootProps()}>
