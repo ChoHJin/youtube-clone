3528ab1e4fd41d07cce1a1c04fd6b5183cb461ec
ChoHJin <cjc9739@naver.com>
2020-08-19 17:14:03 +0900

    comment
    현재 로그에 댓글찍힘

--- boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -3,7 +3,7 @@ import { Row, Col, List, Avatar} from 'antd';
 import Axios from 'axios';
 import SideVideo from './Sections/SideVideo';
 import Subscribe from './Sections/Subscribe';
-
+import Comment from './Sections/Comment';
 
 function VideoDetailPage(props) {
 
@@ -12,6 +12,7 @@ function VideoDetailPage(props) {
     const variable = { videoId : videoId };
 
     const [VideoDetail, setVideoDetail] = useState([]);
+
 
     useEffect(() => {
         
@@ -29,6 +30,7 @@ function VideoDetailPage(props) {
     if(VideoDetail.writer) {
 
         const subscribeButton = VideoDetail.writer._id !== localStorage.getItem('userId') && <Subscribe userTo={ VideoDetail.writer._id } userFrom={ localStorage.getItem('userId') } />
+
         return ( 
             <Row  gutter = {[16, 16]}>
                 <Col lg = {18} xs = {24}>
@@ -47,6 +49,7 @@ function VideoDetailPage(props) {
                     </List.Item>
     
                     {/*comment */}
+                    <Comment postId={videoId} />
     
                 </div>
     
