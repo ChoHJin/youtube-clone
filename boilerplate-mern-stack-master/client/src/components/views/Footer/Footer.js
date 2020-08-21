98ee814ac8046c014d13288ba041655b1672dedb
ChoHJin <cjc9739@naver.com>
2020-08-21 17:29:06 +0900

    Footer css수정중

--- boilerplate-mern-stack-master/client/src/components/views/Footer/Footer.js
+++ boilerplate-mern-stack-master/client/src/components/views/Footer/Footer.js
@@ -8,7 +8,9 @@ function Footer() {
             flexDirection: 'column', alignItems: 'center',
             justifyContent: 'center', fontSize:'1rem'
         }}>
-           <p> Happy Coding  <Icon type="smile" /></p>
+           <p><Icon type="youtube" /> YouTube Clone Coding by 
+           <Icon type="github" />ChoHJin  </p>
+           
         </div>
     )
 }
